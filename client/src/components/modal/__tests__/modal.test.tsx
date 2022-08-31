5127c86e3cd452beaa033b756e3686c9417e6514
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-08-31 17:59:07 +0200

    Add modal tests

---
+++ client/src/components/modal/__tests__/modal.test.tsx
@@ -0,0 +1,21 @@
+import { h } from 'preact';
+import Modal from '../modal';
+import { render, screen } from '@testing-library/preact';
+import '@testing-library/jest-dom';
+
+describe('modal component', () => {
+  beforeEach(() => {
+    render(
+      <Modal title="Title">
+        <div>Hello!</div>
+      </Modal>
+    );
+  });
+  it('should render title', async () => {
+    expect(await screen.findByText(/Title/)).toBeVisible();
+  });
+
+  it('should render children', async () => {
+    expect(await screen.findByText(/Hello!/)).toBeVisible();
+  });
+});
