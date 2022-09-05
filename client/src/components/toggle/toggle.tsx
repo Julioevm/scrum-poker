527a564efbfc498f31f0068d52894d343d9a1fc6
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2022-09-05 14:47:48 +0200

    Implement dark mode toggle and tweak some css classes.

--- client/src/components/toggle/toggle.tsx
+++ client/src/components/toggle/toggle.tsx
@@ -1,16 +1,35 @@
-import { FunctionalComponent, h } from 'preact';
+import { Fragment, FunctionalComponent, h } from 'preact';
+import { useState } from 'preact/hooks';
 import React from 'react';
 import { themeStore } from '../app';
-//import style from './style.css';
+import style from './style.css';
 
 const Toggle: FunctionalComponent = () => {
   const theme = themeStore.getState().theme;
+  const [isDark, setIsDark] = useState(theme === 'dark');
   const switchTheme = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark';
     themeStore.setState({ theme: newTheme });
+    setIsDark(!isDark);
   };
 
-  return <button onClick={switchTheme}>Toggle</button>;
+  return (
+    <Fragment>
+      {isDark ? '🌞' : '🌙'}
+      <div className={style.switchWrapper}>
+        <input
+          checked={isDark}
+          onChange={switchTheme}
+          className={style.switchCheckbox}
+          id={'switch'}
+          type="checkbox"
+        />
+        <label className={style.switchLabel} htmlFor={`switch`}>
+          <span className={style.switchButton} />
+        </label>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Toggle;
